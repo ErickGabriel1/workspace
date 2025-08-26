c3f77fef044c6324e91c9d471a8ec0d3673db5f6
Erick Gabriel <erickgabrielcardosooliveira@gmail.com>
2025-08-26 14:33:01 -0300

    Delete EX9.py

--- PYTHON/EXERCÍCIOS/EX9.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-pygame.init()
-pygame.mixer.music.load("test.mp3")
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy():
-    pygame.time.Clock().tick(10)
-print("--------------------------------")
