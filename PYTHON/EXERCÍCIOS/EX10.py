25926be1a3c7877cd26a4a8d2e6e8f5282b249c3
Erick Gabriel <erickgabrielcardosooliveira@gmail.com>
2025-08-26 14:32:30 -0300

    Delete EX10.py

--- PYTHON/EXERCÍCIOS/EX10.py
+++
@@ -1,7 +0,0 @@
-name = input("Enter your name:").strip()
-
-print(f"your name in uppercase is:{name.upper()}")
-print(f"your name in lowercase is:{name.lower()}")
-print(f"your name has {len(name.replace(" ", ""))} characters")
-print("--------------------------------")
-print(f"your first name is {name.split()[0]} and it has {len(name.split()[0])} characters")
