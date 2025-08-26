a8428337281700cfd6e1d535e54da305704e3f79
Erick Gabriel <erickgabrielcardosooliveira@gmail.com>
2025-08-26 14:33:45 -0300

    Create main.c

---
+++ C/EXERCICIOS/EX2/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+float altura = 0.0;
+float peso = 0.0;
+
+	printf("Digite a sua altura:\n");
+	scanf("%f", &altura);
+
+	printf("Digite o seu peso:\n");
+	scanf("%f", &peso);
+
+	printf("Disciplina: Algoritmo e Programacao Estruturada\n");
+	printf("Turma: GPE17N20241\n");
+	printf("Matricula: UC25103663\n");
+	printf("Nome: Erick Gabriel Cardoso Oliveira\n");
+
+	printf("Altura: %f\n", altura);
+	printf("Altura: %f\n", peso);
+
+	return 0;
+
+}
