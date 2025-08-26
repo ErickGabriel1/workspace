cb48e3a5e0d5d02d4915c4410238aede0d12f6c6
Erick Gabriel <erickgabrielcardosooliveira@gmail.com>
2025-08-26 13:53:08 -0300

    Create main.c

---
+++ C/EXERCÍCIOS/EX2/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+
+	printf('Disciplina: Algoritmo e Programação Estruturada\n');
+	printf('Turma: GPE17N20241\n');
+	printf('Matricula: UC25103663\n');
+	printf('Nome: Erick Gabriel Cardoso Oliveira\n');
+	return 0;
+
+}
+
+1) Abrir o CMD na pasta onde está o código fonte
+2) gcc main.c -o main (Compilar)
+3) ./main (Executar)
