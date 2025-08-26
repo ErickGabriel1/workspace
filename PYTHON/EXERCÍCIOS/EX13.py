6f280f1b7c3fca14b194536cef4bc70b704d8feb
Erick Gabriel <erickgabrielcardosooliveira@gmail.com>
2025-08-26 14:32:37 -0300

    Delete EX13.py

--- PYTHON/EXERCÍCIOS/EX13.py
+++
@@ -1,8 +0,0 @@
-n1 = float(input('Digite o valor a:'))
-n2 = float(input('Digite o valor b:'))
-n3 = float(input('Digite o valor c:'))
-
-if n1 < n2+n3 and n2 < n3+n1 and n3 < n2+n1:
-    print("É um triâmgulo")
-else:
-    print('Não é um triângulo')
