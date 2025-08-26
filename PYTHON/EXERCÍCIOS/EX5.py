388fd3ba7d504ccca0213f570ed019f5ea7e117e
Erick Gabriel <erickgabrielcardosooliveira@gmail.com>
2025-08-26 14:32:47 -0300

    Delete EX5.py

--- PYTHON/EXERCÍCIOS/EX5.py
+++
@@ -1,5 +0,0 @@
-n1 = float(input("Digite a primeira nota:"))
-n2 = float(input("Digite a segnda nota:"))
-media = (n1 + n2) / 2
-
-print("A média entre {} e {} é igual a {:.2f}".format(n1, n2, media))
