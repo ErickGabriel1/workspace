4da3455370db193448b83996f70371c8258293f5
Erick Gabriel <erickgabrielcardosooliveira@gmail.com>
2025-08-26 14:32:54 -0300

    Delete EX7.py

--- PYTHON/EXERCÍCIOS/EX7.py
+++
@@ -1,9 +0,0 @@
-n1 = int(input("Digite um número inteiro:"))
-c = 1
-print("--------------------------------")
-
-while c <= 10:
-    print("{} x {:2} = {}".format(n1, c, n1*c))
-    c = c+1
-else:
-    print("--------------------------------")
