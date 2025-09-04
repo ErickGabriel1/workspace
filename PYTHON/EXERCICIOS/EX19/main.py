79cacaa67a65dfcc2109fb99a1e83fb3bfdcce5a
Erick Gabriel <erickgabrielcardosooliveira@gmail.com>
2025-09-04 19:29:34 -0300

    Prevent repeated letter guesses in hangman game
    Added a check to ensure that previously guessed letters cannot be entered again. Now, if a user tries a letter already used, a message is shown and no life is lost. Also, the secret word is revealed at the end of the game.

--- PYTHON/EXERCICIOS/EX19/main.py
+++ PYTHON/EXERCICIOS/EX19/main.py
@@ -20,26 +20,32 @@ while life != 0:
     print(f'Letras utilizadas: {used}')
     print(f'Palavra: {secret_list}\n')
     char = str(input("Digite uma palava: ")[0].lower())
-
-    hit = 0
-    for c in range(0, len(list_words)):
-        if char == list_words[c]:
-            secret_list[c] = char
-            hit += 1
-    used += char
-    if hit > 0:
-        print(f'\nVocê acertou {hit} letra(s)')
-        time.sleep(0.4)
-        if list_words == secret_list:
-            break
+    if char not in used:
+        hit = 0
+        for c in range(0, len(list_words)):
+            if char == list_words[c]:
+                secret_list[c] = char
+                hit += 1
+        used += char
+        if hit > 0:
+            print(f'\nVocê acertou {hit} letra(s)')
+            time.sleep(0.4)
+            if list_words == secret_list:
+                break
+        else:
+            print('\nVocê errou!')
+            life -= 1
+            time.sleep(0.4)
+        
     else:
-        print('\nVocê errou!')
-        life -= 1
+        print('Essa palavra já foi utilizada!')
         time.sleep(0.4)
 
 if life == 0:
+    print(f'Palavra: {choice}\n')
     print("Você perdeu:")
 else:
+    print(f'Palavra: {choice}\n')
     print("Você ganhou!")
 
 
